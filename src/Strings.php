57bc4c823e647c48955751e2675bd3b3730d2dac
newman101 <jremes@outlook.com>
2015-12-15 19:58:25 +0000

    Refactoring and PSR-2 compliance fixes

--- src/Strings.php
+++ src/Strings.php
@@ -742,7 +742,9 @@ function truncate($input, $length, $chars = '…')
         $splits = preg_split('/([\s\n\r]+)/u', $input, null, PREG_SPLIT_DELIM_CAPTURE);
 
         $splits_length = 0;
-        for ($last_split = 0, $cs = count($splits); $last_split < $cs; ++$last_split) {
+        $splits_count = count($splits);
+
+        for ($last_split = 0; $last_split < $splits_count; ++$last_split) {
             $splits_length += strlen($splits[$last_split]);
             if ($splits_length > $length) {
                 break;
