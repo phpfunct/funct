eac858f062b0821f5110af7e14cc7a313dd76a83
Zhivitsa Kirill <zhivitsa.kirill.gm@gmail.com>
2015-12-18 15:49:12 +0200

    Little optimization for some() function

--- src/Collection.php
+++ src/Collection.php
@@ -516,7 +516,13 @@ function some($collection, callable $callback = null)
         };
     }
 
-    return count(array_filter($collection, $callback)) > 0;
+    foreach ($collection as $item) {
+        if (call_user_func($callback, $item)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 
